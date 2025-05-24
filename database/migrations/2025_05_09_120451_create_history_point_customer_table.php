898dc3aafb4c92f463a72184619088c7e9233dc1
Jack Tran <jacktran210194@gmail.com>
2025-05-24 16:16:10 +0700

    fix bug

--- database/migrations/2025_05_09_120451_create_history_point_customer_table.php
+++ database/migrations/2025_05_09_120451_create_history_point_customer_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('order_code')->index()->nullable();
             $table->string('order_id')->nullable();
             $table->string('title')->index();
-            $table->tinyInteger('point');
+            $table->integer('point');
             $table->timestamps();
         });
     }
